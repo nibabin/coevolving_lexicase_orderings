26a1da5b48ac93773c07935b6fc9a67ad9c72a3a
Claudiu Babin <babinclaudiu@gmail.com>
2022-07-04 22:27:11 -0400

    print valid runs

--- src/propeller/analyze_run.clj
+++ src/propeller/analyze_run.clj
@@ -1,18 +1,19 @@
 (ns propeller.analyze_run)
 
 (def num_runs 50)
-(def folder "with_coevolve")
+(def folder "no_coevolve")
 
 (defn average [coll]
   (float (/ (reduce + coll) (count coll))))
 
-(prn (let [runs (map #(read-string (slurp (clojure.string/join "" ["/home/cbGST/runs/" folder "/run" % ".txt"]))) (range num_runs))
-      successful_runs (filter #(not (= -1 (:success-generation %))) runs)]
-  {:successes (count successful_runs)
-   :generalizations (count (filter #(= 0 (:total-test-error %)) successful_runs))
-   :min-generation (:success-generation (apply min-key :success-generation successful_runs))
-   :max-generation (:success-generation (apply max-key :success-generation successful_runs))
-   :avg-generation (average (map :success-generation successful_runs))
-   :avg-program-executions (average (map #(average (:program-executions %)) successful_runs))
-   }
-  ))
+(prn (let [runs (map #(read-string (slurp (clojure.string/join "" ["/home/cbGST/runs/fuel_cost/" folder "/run" % ".txt"]))) (range num_runs))
+           successful_runs (filter #(not (= -1 (:success-generation %))) runs)]
+       {:total (count runs)
+        :successes (count successful_runs)
+        :generalizations (count (filter #(= 0 (:total-test-error %)) successful_runs))
+        :min-generation (:success-generation (apply min-key :success-generation successful_runs))
+        :max-generation (:success-generation (apply max-key :success-generation successful_runs))
+        :avg-generation (average (map :success-generation successful_runs))
+        :avg-program-executions (average (map #(average (:program-executions %)) successful_runs))
+        }
+       ))
