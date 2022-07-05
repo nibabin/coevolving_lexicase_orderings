7baf727e2b85010735b2cc0f243d375bed24d54d
Claudiu Babin <babinclaudiu@gmail.com>
2022-07-05 12:57:20 -0400

    debug

--- src/propeller/analyze_run.clj
+++ src/propeller/analyze_run.clj
@@ -12,13 +12,15 @@
                             (read-string str)))
                         (range num_runs)))
            successful_runs (filter #(not (= -1 (:success-generation %))) runs)]
-       (prn (count successful_runs))
-       {:total (count runs)
-        :successes (count successful_runs)
-        :generalizations (count (filter #(= 0 (:total-test-error %)) successful_runs))
-        :min-generation (:success-generation (apply min-key :success-generation successful_runs))
-        :max-generation (:success-generation (apply max-key :success-generation successful_runs))
-        :avg-generation (average (map :success-generation successful_runs))
-        :avg-program-executions (average (map #(average (:program-executions %)) successful_runs))
-        }
-       ))
+       (if (= 0 (count successful_runs))
+         {:total (count runs)
+          :successes 0}
+         {:total (count runs)
+          :successes (count successful_runs)
+          :generalizations (count (filter #(= 0 (:total-test-error %)) successful_runs))
+          :min-generation (:success-generation (apply min-key :success-generation successful_runs))
+          :max-generation (:success-generation (apply max-key :success-generation successful_runs))
+          :avg-generation (average (map :success-generation successful_runs))
+          :avg-program-executions (average (map #(average (:program-executions %)) successful_runs))
+          }
+         )))
